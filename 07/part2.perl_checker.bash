ba1f001e0de77512fdf39f60f115750507ab3dcd
Liquid Cat <liquidcat@unknown>
2023-12-07 21:24:54 +0000

    add my Perl helper checks

---
+++ 07/part2.perl_checker.bash
@@ -0,0 +1,1 @@
+tr 'AKQJT' 'EDC1A' | perl -ne '@a=split / /;$a=$a[0];%a=();$a{$_}+=1 for split //,$a;$j=$a{1};delete$a{1};@b=sort{$a{$b}<=>$a{$a}}keys%a;$a{$b[0]}+=$j;@c=@c=sort{$b<=>$a}values%a;if(%a==1){$x=1}elsif(%a==2 && 4==$c[0]){$x=2}elsif(%a==2){$x=3}elsif(3==$c[0]){$x=4}elsif(3==%a){$x=5}elsif(4==%a){$x=6}else{$x=7}$c=%a;@d=sort{$b<=>$a}values%a;print"@{[$x]}:$_"'
