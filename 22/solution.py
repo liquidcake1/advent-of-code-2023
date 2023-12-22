9030e06c236d4b9c3ea55e52bd5b3917fc3b4330
Liquid Cat <liquidcat@unknown>
2023-12-22 13:19:40 +0000

    tidy

--- 22/solution.py
+++ 22/solution.py
@@ -1,8 +1,6 @@
 import sys
 import numpy
-print(sys.argv[1])
 lines = [[numpy.array(list(map(int,y.split(",")))) for y in x.split("~")] for x in open(sys.argv[1], 'r').read().split("\n") if x]
-print(lines)
 lines.sort(key=lambda x: x[1][2])
 heights = [[0 for _ in range(max(x[1][0] for x in lines) + 1)] for _ in range(max(x[1][1] for x in lines) + 1)]
 highblocks = [[[] for _ in range(10)] for _ in range(10)]
@@ -10,16 +8,16 @@ useful = set()
 supporters = [set() for _ in range(len(lines))]
 supporter_counts = [0 for _ in range(len(lines))]
 for i, (start, stop) in enumerate(lines):
-    print(start, stop)
-    print(heights)
     delta = (stop - start)
     l = delta.sum()
-    step = delta // l
+    if l > 0:
+        step = delta // l
+    else:
+        step = delta
     curr = start.copy()
     minh = 0
     support = set()
     for _ in range(l + 1):
-        print(curr, minh)
         h = heights[curr[0]][curr[1]] + 1
         if h > minh:
             support = set()
@@ -34,13 +32,13 @@ for i, (start, stop) in enumerate(lines):
     support = list(support)
     if len(support) == 1:
         useful.update(support)
-    seen = set()
     support_set = set()
+    # Intersect the support nodes of all direct supporters
     if support:
         support_set.update(supporters[support[0]])
-    for supporter in support:
+    for supporter in support[1:]:
         support_set = support_set.intersection(supporters[supporter])
-        print(f"Support for {i} set to {support_set}")
+    # If there's only one supporter for us, add that to ours, too!
     if len(support) == 1:
         support_set.update(support)
     if support_set:
@@ -49,12 +47,8 @@ for i, (start, stop) in enumerate(lines):
             supporter_counts[supporter] += 1
     curr = start.copy()
     for _ in range(l + 1):
-        print(curr)
         heights[curr[0]][curr[1]] = curr[2]
         highblocks[curr[0]][curr[1]] = [i]
         curr += step
-    print(heights)
-    print(len(lines) - len(useful))
-    print(sum(supporter_counts))
-print(supporter_counts)
-print(sum(supporter_counts))
+print("Part 1", len(lines) - len(useful))
+print("Part 2", sum(supporter_counts))
